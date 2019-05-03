d4f967fe7f127754c37a57e81ee79cb17a047b84
Bartosz Sosna <brtsos@gmail.com>
2019-05-04 00:28:24 +0200

    Adjust patch to version st-0.8.2
    Signed-off-by: Miles Alan <m@milesalan.com>
    This is the keyselect patch

--- config.def.h
+++ config.def.h
@@ -179,6 +179,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+    { TERMMOD,              XK_Escape,      keyboard_select,{ 0 } },
 };
 
 /*
